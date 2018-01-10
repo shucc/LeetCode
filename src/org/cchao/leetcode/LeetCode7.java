a451729a07f18a42d4087a2fce399b2e50469487
shucc <cc@cchao.org>
2018-01-10 15:44:53 +0800

    Update LeetCode 7

--- src/org/cchao/leetcode/LeetCode7.java
+++ src/org/cchao/leetcode/LeetCode7.java
@@ -7,29 +7,27 @@ package org.cchao.leetcode;
 public class LeetCode7 {
 
     public static void main(String[] args) {
+        System.out.println(String.valueOf(reverse(0)));
         System.out.println(String.valueOf(reverse(123)));
         System.out.println(String.valueOf(reverse(-2875)));
         System.out.println(String.valueOf(reverse(120)));
-        System.out.println(String.valueOf(reverse(1646324359)));
+        System.out.println(String.valueOf(reverse(1534236469)));
     }
 
     public static int reverse(int x) {
-        if ( 0 == x) {
+        boolean isNegative = x < 0;
+        x = Math.abs(x);
+        int result = 0;
+        int length = String.valueOf(x).length();
+        while (x > 0) {
+            result += ((x % 10) * Math.pow(10, length - 1));
+            x = x / 10;
+            length = length - 1;
+        }
+        //整数溢出
+        if (result == 2147483647) {
             return 0;
         }
-        boolean isNegative = (x < 0);
-        x = Math.abs(x);
-        String number = String.valueOf(x);
-        int length = number.length();
-        char[] numberArray = new char[length];
-        for (int i = 0; i < length; i++) {
-            numberArray[i] = number.charAt(i);
-        }
-        StringBuilder stringBuilder = new StringBuilder("");
-        for (int i = length - 1; i >= 0; i--) {
-            stringBuilder.append(numberArray[i]);
-        }
-        int result = Integer.valueOf(stringBuilder.toString());
         if (isNegative) {
             return -result;
         }
