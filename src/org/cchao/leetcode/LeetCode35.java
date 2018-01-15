2a7f5f042ecbdd5eb0b542ab61f5d72e44a148ce
shucc <cc@cchao.org>
2018-01-15 14:38:31 +0800

    Add LeetCode 34,35

---
+++ src/org/cchao/leetcode/LeetCode35.java
@@ -0,0 +1,30 @@
+package org.cchao.leetcode;
+
+/**
+ * Created by shucc on 18/1/15.
+ * cc@cchao.org
+ */
+public class LeetCode35 {
+    
+    public static void main(String args[]) {
+        int[] temp1 = new int[] {1,3,5,6};
+        System.out.println(String.valueOf(searchInsert(temp1, 5)));
+        System.out.println(String.valueOf(searchInsert(temp1, 2)));
+        System.out.println(String.valueOf(searchInsert(temp1, 7)));
+        System.out.println(String.valueOf(searchInsert(temp1, 0)));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (target == nums[i]) {
+                return i;
+            }
+            if (nums[i] > target) {
+                if (i == 0 || nums[i - 1] < target) {
+                    return i;
+                }
+            }
+        }
+        return nums.length;
+    }
+}
