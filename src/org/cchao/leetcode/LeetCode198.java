7f83ff87ce8a683b8a060ec257c4979cb7e5cbb8
shucc <cc@cchao.org>
2018-05-23 15:38:37 +0800

    快排🐂逼

---
+++ src/org/cchao/leetcode/LeetCode198.java
@@ -0,0 +1,35 @@
+package org.cchao.leetcode;
+
+/**
+ * Created by shucc on 18/5/23.
+ * cc@cchao.org
+ * 打家劫舍
+ */
+public class LeetCode198 {
+
+    public static void main(String args[]) {
+        int[] test1 = new int[]{2, 7, 9, 3, 1};
+        int[] test2 = new int[]{2, 1, 1, 2};
+        System.out.println(String.valueOf(rob(test1)) + "-->" + rob(test2));
+    }
+
+    public static int rob(int[] nums) {
+        int length = nums.length;
+        if (length == 0) {
+            return 0;
+        }
+        if (length == 1) {
+            return nums[1];
+        }
+        if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int sum[] = new int[length];
+        sum[0] = nums[0];
+        sum[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            sum[i] = Math.max(sum[i - 1], sum[i - 2] + nums[i]);
+        }
+        return sum[length - 1];
+    }
+}
---
+++ src/org/cchao/leetcode/binarytree/IsValidBST.java
@@ -0,0 +1,29 @@
+package org.cchao.leetcode.binarytree;
+
+/**
+ * Created by shucc on 18/5/14.
+ * cc@cchao.org
+ * 判断是否为二叉搜索树
+ */
+public class IsValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MAX_VALUE, Long.MIN_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long max, long min) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        if (root.left != null && root.val <= root.left.val) {
+            return false;
+        }
+        if (root.right != null && root.val >= root.right.val) {
+            return false;
+        }
+        return (root.left == null ? true : isValidBST(root.left, root.val, min)) && (root.right == null ? true : isValidBST(root.right, max, root.val));
+    }
+}
