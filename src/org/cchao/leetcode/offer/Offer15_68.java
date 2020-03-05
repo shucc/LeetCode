ed4a1af4f2135b310291935296bc6dc070c1454c
cchen6 <cchen6@i9i8.com>
2020-03-05 18:05:42 +0800

    Add offer 15,16

---
+++ src/org/cchao/leetcode/offer/Offer15_68.java
@@ -0,0 +1,36 @@
+package org.cchao.leetcode.offer;
+
+/**
+ * @author cchen6
+ * @Date on 2020/3/5
+ * @Description 二叉搜索树的最近公共祖先
+ */
+public class Offer15_68 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+    private TreeNode process(TreeNode root, TreeNode minNode, TreeNode maxNode) {
+        if (null == root) {
+            return null;
+        }
+        if (null == minNode) {
+            return maxNode;
+        }
+        if (null == maxNode) {
+            return minNode;
+        }
+        if (minNode.val <= root.val && maxNode.val >= root.val) {
+            return root;
+        }
+        if (maxNode.val < root.val) {
+            return process(root.left, minNode, maxNode);
+        }
+        return process(root.right, minNode, maxNode);
+    }
+}
---
+++ src/org/cchao/leetcode/offer/Offer16_03.java
@@ -0,0 +1,19 @@
+package org.cchao.leetcode.offer;
+
+/**
+ * @author cchen6
+ * @Date on 2020/3/5
+ * @Description 数组中重复的数字
+ */
+public class Offer16_03 {
+    public int findRepeatNumber(int[] nums) {
+        int[] temp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (temp[nums[i]] > 0) {
+                return nums[i];
+            }
+            temp[nums[i]] = 1;
+        }
+        return 0;
+    }
+}
